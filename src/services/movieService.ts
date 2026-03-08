ed56904be8c12f8a5792684282f346759b60c7e9
fqsocite <denispenzinn@gmail.com>
2026-03-08 13:27:56 +0100

    modified movieService.ts

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -4,7 +4,7 @@ import { Movie } from '../types/movie'
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 export const searchMovies = async (query: string): Promise<Movie[]> => {
-    const response = await axios.get(
+    const response = await axios.get<{ results: Movie[] }>(
         "https://api.themoviedb.org/3/search/movie",
         {
             params: {
